8c4ad41a7b74463c0a6c922b11f478f479fb8b78
RomanVelnar <roman.velnar@gmail.com>
2022-02-09 10:58:36 +0000

    finished with the airbnb experiece project

---
+++ airbnb-experience-clone/src/App.js
@@ -0,0 +1,31 @@
+import React from "react"
+import './App.css';
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+
+function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        item={item}
+      />
+    )
+  })
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+                {cards}
+      </section>  
+    </div>
+  );
+}
+
+export default App;
