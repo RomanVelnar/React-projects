06d1d6d3c479986ea3954e7d099f9df5de9df56f
RomanVelnar <roman.velnar@gmail.com>
2022-02-17 07:16:02 +0000

    Added react-facts with dark-light mode

---
+++ react-facts-dark-light-mode/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
+
+function App() {
+  const [darkMode, setDarkMode] = React.useState(true)
+
+  function toggleDarkMode() {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
+  return (
+    <div className="container">
+        <Navbar 
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+        <Main 
+          darkMode={darkMode}
+        />
+    </div>
+  );
+}
+
+export default App;
